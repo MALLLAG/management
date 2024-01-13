a0141ff44381ed34e46ee44ef3120938ed47bdd6
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2024-01-13 22:08:32 +0900

    feat: 계약 조회 response class 추가

---
+++ src/main/kotlin/com/contract/management/domain/contract/controller/response/ContractResponse.kt
@@ -0,0 +1,23 @@
+package com.contract.management.domain.contract.controller.response
+
+import com.contract.management.domain.coverage.controller.response.CoverageResponse
+import java.math.BigDecimal
+
+data class ContractResponse(
+    val productName: String,
+    val contractPeriod: Int,
+    val coverages: List<CoverageResponse>
+) {
+
+    companion object {
+        fun of(
+            productName: String,
+            contractPeriod: Int,
+            coverages: List<CoverageResponse>
+        ): ContractResponse = ContractResponse(
+            productName = productName,
+            contractPeriod = contractPeriod,
+            coverages = coverages
+        )
+    }
+}
