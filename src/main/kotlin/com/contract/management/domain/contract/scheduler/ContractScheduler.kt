bee4409ee87a948db7e59c191d68555c1572a4bf
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2024-01-14 00:07:11 +0900

    feat: 만료 7일 전 계약 메일 발송 스케줄러 구현

---
+++ src/main/kotlin/com/contract/management/domain/contract/scheduler/ContractScheduler.kt
@@ -0,0 +1,19 @@
+package com.contract.management.domain.contract.scheduler
+
+import com.contract.management.domain.contract.repository.ContractRepository
+import com.contract.management.domain.contract.service.ContractService
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.time.LocalDate
+
+@Component
+class ContractScheduler(
+    private val contractService: ContractService
+) {
+
+    @Scheduled(cron = "0 0 12 * * ?")
+    fun sendMailForExpiringContractsOneWeekBefore() {
+        contractService.findExpiringContractsOneWeekBefore()
+            .forEach { println("계약 번호 : ${it.id}의 계약 만기 일주일 전입니다.") }
+    }
+}
