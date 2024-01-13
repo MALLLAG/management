307e0dda92c7b09a6f858cb30f263bcb39bbb6de
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2024-01-14 00:06:53 +0900

    feat: 만료 7일 전 계약 조회 쿼리 구현

--- src/main/kotlin/com/contract/management/domain/contract/repository/ContractRepository.kt
+++ src/main/kotlin/com/contract/management/domain/contract/repository/ContractRepository.kt
@@ -2,6 +2,19 @@ package com.contract.management.domain.contract.repository
 
 import com.contract.management.domain.contract.entity.Contract
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.time.LocalDate
 
 interface ContractRepository: JpaRepository<Contract, Long> {
+    @Query(
+        value = """
+            SELECT * 
+            FROM contract c 
+            WHERE c.insurance_end_date = TIMESTAMPADD(DAY, 7, CURRENT_DATE)
+        """,
+        nativeQuery = true
+    )
+    fun findExpiringContractsOneWeekBefore(): List<Contract>
+
 }
