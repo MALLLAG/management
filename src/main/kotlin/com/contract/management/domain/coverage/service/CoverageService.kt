57f2e002046d7fbc07843fbe2d4dc9c1594c4586
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2024-01-13 22:03:21 +0900

    feat: 담보 생성, 담보 기반 보험료 계산 로직 구현

---
+++ src/main/kotlin/com/contract/management/domain/coverage/service/CoverageService.kt
@@ -0,0 +1,59 @@
+package com.contract.management.domain.coverage.service
+
+import com.contract.management.domain.coverage.controller.request.CoverageSaveRequest
+import com.contract.management.domain.coverage.entity.Coverage
+import com.contract.management.domain.coverage.entity.calculateAmount
+import com.contract.management.domain.coverage.entity.isIncludedInProduct
+import com.contract.management.domain.coverage.repository.CoverageRepository
+import com.contract.management.global.exception.BusinessException
+import com.contract.management.global.exception.ResponseCode
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.math.BigDecimal
+
+@Service
+@Transactional
+class CoverageService(
+    private val coverageRepository: CoverageRepository
+) {
+
+    fun saveCoverage(
+        request: CoverageSaveRequest
+    ): Long {
+        validateDuplicateCoverage(request)
+
+        val coverage = coverageRepository.save(Coverage.from(request))
+        return coverage.id
+    }
+
+    private fun validateDuplicateCoverage(
+        request: CoverageSaveRequest
+    ) {
+        if (coverageRepository.existsByName(request.name)) {
+            throw BusinessException(ResponseCode.DUPLICATE_COVERAGE)
+        }
+    }
+
+    @Transactional(readOnly = true)
+    fun calculateAmount(
+        productId: Long,
+        contractPeriod: Int,
+        coverageIds: List<Long>
+    ): BigDecimal {
+        return coverageRepository.findAllById(coverageIds)
+            .takeIf { it.isIncludedInProduct(productId) }
+            ?.calculateAmount(contractPeriod)
+            ?: throw BusinessException(ResponseCode.NOT_INCLUDED_IN_PRODUCT)
+    }
+
+    fun validateIncludedInProduct(
+        coverageIds: List<Long>,
+        productId: Long
+    ) {
+        val coverages = coverageRepository.findAllById(coverageIds)
+
+        if (!coverages.isIncludedInProduct(productId)) {
+            throw BusinessException(ResponseCode.NOT_INCLUDED_IN_PRODUCT)
+        }
+    }
+}
