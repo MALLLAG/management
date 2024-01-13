7f7d592d9d524bae075a86d4bb415348687f23b6
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2024-01-13 22:10:05 +0900

    test: 계약 dummy data 생성을 위한 factory class 추가

---
+++ src/test/kotlin/com/contract/management/factory/ContractFactory.kt
@@ -0,0 +1,24 @@
+package com.contract.management.factory
+
+import com.contract.management.domain.contract.controller.request.ContractModifyRequest
+import com.contract.management.domain.contract.controller.request.ContractSaveRequest
+import com.contract.management.domain.contract.entity.Contract
+import com.contract.management.domain.contract.entity.enums.ContractStatus
+import java.math.BigDecimal
+import java.time.YearMonth
+
+class ContractFactory {
+
+    companion object {
+        fun createContract(
+            productId: Long
+        ): Contract = Contract(
+            productId = productId,
+            period = 12,
+            totalAmount = BigDecimal("1000000"),
+            insuranceStartDate = YearMonth.of(2023, 10),
+            insuranceEndDate = YearMonth.of(2024, 6),
+            status = ContractStatus.NORMAL
+        )
+    }
+}
