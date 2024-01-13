53b6a47f6c1654735576c6c2094796836f304f55
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2024-01-13 22:10:46 +0900

    test: ContractStatus String -> enum 변환 함수 테스트 코드 구현

---
+++ src/test/kotlin/com/contract/management/domain/contract/entity/enums/ContractStatusTest.kt
@@ -0,0 +1,25 @@
+package com.contract.management.domain.contract.entity.enums
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
+class ContractStatusTest {
+
+    @Nested
+    class `ContractStatus의 from 함수는` {
+
+        @Test
+        fun `String의 status 값을 받아, enum으로 변환해서 반환한다`() {
+            // given
+            val status = "NORMAL"
+
+            // when
+            val contractStatus = ContractStatus.from(status)
+
+            // then
+            assertThat(contractStatus).isEqualTo(ContractStatus.NORMAL)
+        }
+    }
+}
